ea76fddc8dd5b409a9827e13da37bf0279aa3020
lab1 <zhanerketkva7@gmail.com>
2023-04-09 20:20:17 +0600

    comments

--- src/Main.java
+++ src/Main.java
@@ -48,17 +48,33 @@ public class Main{
 
         }
     }
-
+    /**
+     Finds the minimum value in the given integer array.
+     arr=The integer array to find the minimum value of.
+     n=The number of elements in the array.
+     return=The minimum value in the array.
+     */
     public static float findSum(int n, int[]arr) {
         int a = arr[n - 1];
         if (n == 1) return a;
         return a + findSum(n - 1, arr);
     }
+    /**
+     Finds the sum of the elements in the given integer array.
+     n=The number of elements in the array.
+     arr=The integer array to find the sum of.
+     return=The sum of the elements in the array.
+     */
     public static float findAverage(int n, int[] arr) {
         float avr = findSum(Integer.valueOf(n), arr);
         return avr / n;
     }
-
+    /**
+     Finds the average value of the elements in the given integer array.
+     n=The number of elements in the array.
+     arr=The integer array to find the average of.
+     return=The average value of the elements in the array.
+     */
     public static boolean checkPrime(int n) {
         boolean isprime=true;
         if (n < 2)
@@ -77,6 +93,11 @@ public class Main{
             else System.out.printf("Composite", n);
         }
         return isprime;}
+    /**
+     Checks whether the given integer is a prime number.
+     n=The integer to check.
+     return=true if the integer is prime, false otherwise.
+     */
     public static int findFactorial(int n){
         if(n==1){
             return 1;
@@ -84,6 +105,11 @@ public class Main{
             return n*findFactorial(n-1);
         }
     }
+    /**
+     Calculates the factorial of the given integer.
+     n=The integer to calculate the factorial of.
+     return=The factorial of the integer.
+     */
     public static int findFibonacci(int n) {
         return switch(n){
             case 0->0;
@@ -91,7 +117,11 @@ public class Main{
             default-> findFibonacci(n-1)+findFibonacci(n-2);
         };
     }
-
+    /**
+     Calculates the nth Fibonacci number recursively using a switch statement.
+     n=The index of the desired Fibonacci number.
+     return=The nth Fibonacci number.
+     */
     public static int findPower(int a, int n) {
         int pow=1;
         for (int i = 0; i < n; i++) {
@@ -99,6 +129,12 @@ public class Main{
         }
         return pow;
     }
+    /**
+     Calculates the power of a number a to the exponent n.
+     a=The base number.
+     n=The exponent.
+     return=The result of a raised to the power of n.
+     */
     public static boolean checkIsDigit(String s){
         for(int i=0; i<s.length(); i++){
         if(Character.isDigit(s.charAt(i))){
@@ -107,6 +143,11 @@ public class Main{
         }
         return true;
     }
+    /**
+     checks whether a given string consists only of digits or not.
+     s=the string to be checked
+     return=true if the string consists only of digits, false otherwise
+     */
     public static int findBinomCoeff(int a, int n) {
         if (n == 0 || n == a) {
         return 1;
@@ -116,6 +157,12 @@ public class Main{
         return binomial;
     }
     }
+    /**
+     Calculates the binomial coefficient
+     a=the total number of elements to choose from
+     n=the number of elements to choose
+     return=the binomial coefficient of (a choose n)
+     */
 
 }
 
